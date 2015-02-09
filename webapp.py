71bab64f613c4b80d82a63aa85fe7d84f4704de9
kr4ng <steven.m.simoni@gmail.com>
2015-02-09 15:59:03 -0800

    rest api scaffolding

--- webapp.py
+++ webapp.py
@@ -1,8 +1,36 @@
 import os
 from flask import Flask
+from flask.ext import restful
+
+import httplib
+import logging
+import requests
+from restmarketo import Client
 
 app = Flask(__name__)
+api = restful.Api(app)
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+class Merge(restful.Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(Merge, '/merge')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    httplib.HTTPConnection.debuglevel = 1
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
+    requests.get('http://httpbin.org/headers')
+
+    client=Client('161-TPY-810', 'dc499150-316a-4a49-9700-19f9e2e130b3', 'r656aoxoK6WW6U1ev60KA5c6s3PemnZK')
+    
+    client.createUpdateLeads([{'email':'testy@testerson.org', 'firstName':'testy','lastName':'testerson'}])
