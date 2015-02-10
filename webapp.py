f9a59f32524f2cc021e72919957574f3ca35cfb6
kr4ng <steven.m.simoni@gmail.com>
2015-02-09 21:25:08 -0800

    working merge script

--- webapp.py
+++ webapp.py
@@ -10,31 +10,54 @@ from restmarketo import Client
 app = Flask(__name__)
 api = restful.Api(app)
 
+# logging
+httplib.HTTPConnection.debuglevel = 1
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+requests.get('http://httpbin.org/headers')
+
+# just a route
 @app.route('/')
 def hello():
     return 'Hello World!'
 
+# this is the main part of the app - this is a rest api.  the webhook from marketo hits this script.
 class Merge(restful.Resource):
     def post(self):
         json_input = request.get_json(force=True)
-        print json_input
-        return {'hello': 'world'}
+        email = json_input['email']
+        client = Client('973-AGY-801','f0f62b61-9545-4c43-b1f5-c2c16a50361f','YQ2hi3qPYiwzfeUrJRa9LUQss9lx3VMU')
+        leads = client.getMultipleLeadsByFilterType('email', [email], fields=['email','firstName'])
+        # find the lead that should be the winner
+        try:
+            winningLeadId = leads[1]['id']
+            losingLeadId = leads[0]['id']
+            client.mergeLeads(winningLeadId, losingLeadId)
+        except:
+            pass
+        return leads
 
+'''
+# this may not be necessary since we only have 2 leads in this use case.
+def determineWinningLead(leads):
+    # Takes in a json array of leads and returns the winning lead
+    # based on the one that is last created, which will be the SFDC one
+    # initialize a winning lead
+    winningLead = leads[0]
+    for lead in leads:
+        if winningLead['id'] < lead['id']:
+            winningLead = lead
+    return winningLead
+'''
 api.add_resource(Merge, '/merge')
 
 if __name__ == '__main__':
     app.run(debug=True)
     print 'app loaded'
     '''
-    httplib.HTTPConnection.debuglevel = 1
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.DEBUG)
-    requests_log = logging.getLogger("requests.packages.urllib3")
-    requests_log.setLevel(logging.DEBUG)
-    requests_log.propagate = True
-    requests.get('http://httpbin.org/headers')
-
     client=Client('161-TPY-810', 'dc499150-316a-4a49-9700-19f9e2e130b3', 'r656aoxoK6WW6U1ev60KA5c6s3PemnZK')
-    
     client.createUpdateLeads([{'email':'testy@testerson.org', 'firstName':'testy','lastName':'testerson'}])
     '''
